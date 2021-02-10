732a170743777baf2a3ec10df1a61b882f6f4794
aldomozhirov <aldomozhirov@gmail.com>
2021-02-10 23:25:37 +0200

    Translate to Russian. Added info message.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Вы на ветке main. Этот код готов пойти в production!
+        </p>
+        <p>
+          Редактируйте <code>src/App.js</code> и сохраните для обновления.
         </p>
         <a
           className="App-link"
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Изучаем реакт!
         </a>
       </header>
     </div>
